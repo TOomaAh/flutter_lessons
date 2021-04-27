f9b27ad6b79f47896d5ef25870e5e893d16bcf89
THOMAS B <tbrunet72@gmail.com>
2021-04-27 14:44:26 +0200

    Resolve Exo 1

--- lib/exercise/exercise.dart
+++ lib/exercise/exercise.dart
@@ -6,3 +6,39 @@
 /// * MaterialApp
 /// * InheritedWidget concept
 /// * Theme
+import "package:flutter/material.dart";
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Home(),
+      theme: new ThemeData(
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            color: Colors.amber,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Container(
+        child: new Text(
+          "Hello",
+          style: Theme.of(context).textTheme.headline1,
+        ),
+      ),
+    );
+  }
+}
